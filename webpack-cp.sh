2ca5e20b74f8dbc0d24b177d54d2b493b53afe55
Deniz Cengiz <d.c.cengiz@googlemail.com>
2025-06-02 23:49:04 +0200

    Change folder name to Trio from iAPS

--- webpack-cp.sh
+++ webpack-cp.sh
@@ -21,14 +21,14 @@
 npx webpack
 
 #
-#  --- Copy and rename oref0/dist .js files to FreeAPS/Resources/javascript/bundle ---
+#  --- Copy and rename oref0/dist .js files to Trio/Resources/javascript/bundle ---
 #
 # change directory variables as needed:
 oref0DIR=./
 apsDIR=../Trio
 
 
-bundleDIR=$apsDIR/FreeAPS/Resources/javascript/bundle
+bundleDIR=$apsDIR/Trio/Resources/javascript/bundle
 
 cp -p -v $oref0DIR/dist/bundle/*.js $bundleDIR/
 
@@ -39,7 +39,7 @@ echo ""
 
 cp -p -R $oref0DIR/lib $apsDIR/trio-oref/
 echo "These source files are copied from https://github.com/nightscout/trio-oref, and are for information purposes only." > $apsDIR/trio-oref/oref_source_file_info.txt
-echo "The algorithm is run based on minimised files in FreeAPS/Resources/javascript/bundle." >> $apsDIR/trio-oref/oref_source_file_info.txt
+echo "The algorithm is run based on minimised files in Trio/Resources/javascript/bundle." >> $apsDIR/trio-oref/oref_source_file_info.txt
 
 # Retrieves version, branch, and tag information from Git
 git_version=$(git log -1 --format="%h" --abbrev=7)
